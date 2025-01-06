0d42e8b78bb6be59c45d0eae8b867a43a25bdbaf
Artabarean <artabarean@gmail.com>
2025-01-06 13:36:03 +0100

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+#include "libft.h"
+//#include <stddef.h>
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*ptr1;
+	char	*ptr2;
+	size_t	i;
+
+	ptr1 = (char *)s1;
+	ptr2 = (char *)s2;
+	while (i < n && ptr[i] != '\0' && ptr2[i] != '\0')
+	{
+		if(ptr1[i] < ptr2[i])
+		{
+			return (ptr1[i] - ptr2[i]);
+		}
+		else if(ptr1[i] > ptr2[i])
+		{
+			return (ptr1[i] - ptr2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+/*#include <stdio.h>
+int main()
+{
+	printf("el resultado de la comparativa es = %d\n", ft_memcmp("Holapapope", "HolaPapope", 10));
+		       return (0);
+}
+*/
